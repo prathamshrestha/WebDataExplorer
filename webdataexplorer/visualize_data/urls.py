1437e3557919c6525601085d8d9c66a0702bc139
prathamshrestha <prathamshrestha358@gmail.com>
2023-09-18 20:36:24 +0545

    readme and urls modifications

--- webdataexplorer/visualize_data/urls.py
+++ webdataexplorer/visualize_data/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from .views import VisualizeView, ScrapePostView
 
 
-# TODO: 3. READme make more clear about the purpose of the project
 urlpatterns = [
     path("scrape_data_list/", VisualizeView.as_view(), name="visualize data"),
     path("api/item/", ScrapePostView.as_view(), name="create data"),
